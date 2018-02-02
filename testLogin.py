ee52f02c0b70e722fc4900ff86ef33c7e138440d
leskhantos <33034419+leskhantos@users.noreply.github.com>
2018-02-02 10:17:18 +0600

    Add files via upload

---
+++ testLogin.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
+driver = webdriver.Firefox()
+
+driver.get("https://dev.e-sauda.kz/login?entry_type=manual")
+class TestingDEV(unittest.TestCase):
+ def LoginTo(self):
+      comp = driver.find_element_by_id('loginform-email')
+      comp.send_keys("leskhan001@gmail.com")
+      comp.send_keys(Keys.TAB)
+      pas = driver.find_element_by_id('loginform-password')
+      pas.send_keys("123456")
+      pas.send_keys(Keys.RETURN)
+      assert("Открытые")
+
+ 
+ 
+ def Create():
+    create = driver.find_element_by_link_text('Создать')
+    create.click()
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
