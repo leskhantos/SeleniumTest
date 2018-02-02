2ad4de1345ce9187ec7440ba4fe957dfb1ded787
leskhantos <33034419+leskhantos@users.noreply.github.com>
2018-02-02 10:24:13 +0600

    Add files via upload
    Login code without test result

--- testLogin.py
+++ testLogin.py
@@ -4,8 +4,8 @@ import unittest
 driver = webdriver.Firefox()
 
 driver.get("https://dev.e-sauda.kz/login?entry_type=manual")
-class TestingDEV(unittest.TestCase):
- def LoginTo(self):
+
+def LoginTo():
       comp = driver.find_element_by_id('loginform-email')
       comp.send_keys("leskhan001@gmail.com")
       comp.send_keys(Keys.TAB)
@@ -13,14 +13,4 @@ class TestingDEV(unittest.TestCase):
       pas.send_keys("123456")
       pas.send_keys(Keys.RETURN)
       assert("Открытые")
-
- 
- 
- def Create():
-    create = driver.find_element_by_link_text('Создать')
-    create.click()
-    
-
-
-if __name__ == '__main__':
-    unittest.main()
+LoginTo()
