e13dee7744d7cdcd693734d9df0b8f47e83a864f
leskhantos <33034419+leskhantos@users.noreply.github.com>
2018-02-28 20:29:05 +0600

    Update CabTest.py

--- CabTest.py
+++ CabTest.py
@@ -9,10 +9,10 @@ driver = webdriver.Firefox()
 driver.get("https://dev.e-sauda.kz/login?entry_type=manual")
 def LoginTo():
       comp = driver.find_element_by_id('loginform-email')
-      comp.send_keys("leskhan001@gmail.com")
+      comp.send_keys("user")
       comp.send_keys(Keys.TAB)
       pas = driver.find_element_by_id('loginform-password')
-      pas.send_keys("123456")
+      pas.send_keys("pass")
       pas.send_keys(Keys.RETURN)
       time.sleep(5)
 
