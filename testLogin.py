0d97095c07bfc480beb4e090ebb1cb96be0a805a
leskhantos <33034419+leskhantos@users.noreply.github.com>
2018-02-28 20:28:28 +0600

    Update testLogin.py

--- testLogin.py
+++ testLogin.py
@@ -7,7 +7,7 @@ driver.get("https://dev.e-sauda.kz/login?entry_type=manual")
 
 def LoginTo():
       comp = driver.find_element_by_id('loginform-email')
-      comp.send_keys("leskhan001@gmail.com")
+      comp.send_keys("usermail")
       comp.send_keys(Keys.TAB)
       pas = driver.find_element_by_id('loginform-password')
       pas.send_keys("paaaaaasssssswwwwwoooorrrrrdddddd")
