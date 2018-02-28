28df1f3e516c2842a645800196f80d2b53b37637
leskhantos <33034419+leskhantos@users.noreply.github.com>
2018-02-28 20:28:02 +0600

    Update testLoginRes.py

--- testLoginRes.py
+++ testLoginRes.py
@@ -7,10 +7,10 @@ driver.get("https://dev.e-sauda.kz/login?entry_type=manual")
 class TestingDEV(unittest.TestCase):
  def LoginTo():
       comp = driver.find_element_by_id('loginform-email')
-      comp.send_keys("leskhan001@gmail.com")
+      comp.send_keys("username")
       comp.send_keys(Keys.TAB)
       pas = driver.find_element_by_id('loginform-password')
-      pas.send_keys("123456")
+      pas.send_keys("pass")
       pas.send_keys(Keys.RETURN)
       assert("Открытые")
       
